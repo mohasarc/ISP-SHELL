e3dd7cbe4683299dbbec532c3fd6cd311a9987a8
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2021-02-27 18:53:50 +0300

    implemented the normal mode

--- isp.c
+++ isp.c
@@ -5,15 +5,23 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 const char PIPE_CHAR = '|';
+#define FALSE 0
+#define TRUE 1
+#define READ 0 // The index of the read end of the unnamed pipe
+#define WRITE 1 // The index of the write end of the unnamed pipe
+#define MAX_PIPE_COUNT 100 // The maximun number of normal pipes can be handled
+int fd[MAX_PIPE_COUNT][2]; // Stores the file descriptors of all unnamed pipes opened
+int fd2[MAX_PIPE_COUNT][2]; // Stores the file descriptors of all unnamed pipes opened
 
 // Prototypes
 void tokenizeString(char args[500], int *argc, char *argv[]);
 void parseInput(int argc, char *argv[], char cmds[][50], int *cmdsc);
-void runChildProcess(char *cmd);
+void runChildProcess(char *cmd, int pipeRead, int pipeWrite, int readFd[2], int writeFd[2]);
 
 // The code
 int main()
 {
+
     while (1){
         int argc = 0;
         int cmdsc = 0;
@@ -26,8 +34,37 @@ int main()
         tokenizeString(args, &argc, argv);
         parseInput(argc, argv, cmds, &cmdsc);
 
+        // Using the normal pipe
         for (int i = 0; i < cmdsc; i++){
-            runChildProcess(cmds[i]);
+            if (cmdsc == 1){ // There is only one process, no pipe is needed
+                runChildProcess(cmds[i], FALSE, FALSE, NULL, NULL);
+            } 
+            else { // Some pipes are needed
+                if (i == (cmdsc - 1)){ // The last child will only read
+                    runChildProcess(cmds[i], TRUE, FALSE, fd[i-1], NULL);
+                    
+                    close(fd[i-1][0]);
+                    close(fd[i-1][1]);
+                } 
+                else {
+                    // Need to create a pipe to write to
+                    pipe (fd[i]);
+
+                    if (i == 0){ // Will only write to next pipe
+                        runChildProcess(cmds[i], FALSE, TRUE, NULL, fd[i]);
+                    } 
+                    else { // Will read from previous pipe and write to next pipe
+                        runChildProcess(cmds[i], TRUE, TRUE, fd[i-1], fd[i]);
+
+                        close(fd[i-1][0]);
+                        close(fd[i-1][1]);
+                    }
+                }
+            }
+        }
+        
+        for (int i = 0; i < cmdsc; i ++){
+            wait(NULL);
         }
     }
 }
@@ -75,22 +112,44 @@ void parseInput(int argc, char *argv[], char cmds[][50], int *cmdsc){
     *cmdsc = cmdCount + 1;
 }
 
-void runChildProcess(char *cmd){
+/**
+ * Executes a command in string format
+ * @param cmd The command in string format
+ */
+void runChildProcess(char *cmd, int pipeRead, int pipeWrite, int readFd[2], int writeFd[2]){
     pid_t pid;
     int status;
 
     pid = fork ();
 
-    if (pid < 0)
+    if (pid < 0){
+        fprintf(stderr, "Fork kailed\n");
         return;
+    }
 
     if (pid == 0){
-        // In the child process
+        /*
+            Handling pipe initialization
+        */
+        if (pipeRead){
+            close (readFd[WRITE]); /* Close unused end*/ 
+            dup2(readFd[READ], STDIN_FILENO);
+        }
+
+        if (pipeWrite){
+            close (writeFd[READ]); /* Close unused end*/
+            dup2(writeFd[WRITE], STDOUT_FILENO);
+        }
+
+        /*
+            Execute the wanted command
+        */
         int argc = 0;
-        char *argv[100] = {'\0'};
+        char *argv[100];
         tokenizeString(cmd, &argc, argv);
-        execvp(argv[0], argv);
-    } else {
-        wait(&status);
+        if (execvp(argv[0], argv)){
+            fprintf(stderr, "Failed executing command!\n\n");
+            exit(1);
+        }
     }
 }
